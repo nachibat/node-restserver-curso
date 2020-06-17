e0cdbf5bb6daaca9506227ee772ec8f75a655b00
nachi2083 <nachi2083@gmail.com>
2020-06-17 20:05:03 -0300

    Subida de archivo

---
+++ server/routes/upload.js
@@ -0,0 +1,166 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
+
+const Usuario = require('../models/usuario');
+const Producto = require('../models/producto');
+
+const fs = require('fs');
+const path = require('path');
+
+// Default Options
+app.use(fileUpload());
+
+app.put('/upload/:tipo/:id', (req, res) => {
+
+    const tipo = req.params.tipo;
+    const id = req.params.id;
+
+    if (!req.files) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'No se ha seleccionado ningún archivo'
+            }
+        });
+    }
+
+    // Validar tipo
+    const tiposValidos = ['productos', 'usuarios'];
+
+    if (tiposValidos.indexOf(tipo) < 0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'Los tipos permitidos son ' + tiposValidos.join(', ')
+            }
+        });
+    }
+
+    const archivo = req.files.archivo;
+    const nombreCortado = archivo.name.split('.');
+    const extension = nombreCortado[nombreCortado.length - 1];
+
+    // Extensiones permitidas
+    const extensionesValidas = ['png', 'jpg', 'gif', 'jpeg'];
+
+    if (extensionesValidas.indexOf(extension) < 0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'Las extensiones permitidas son ' + extensionesValidas.join(', '),
+                ext: extension
+            },
+        });
+    }
+
+    // Cambiar nombre al archivo
+    const nombreArchivo = `${id}-${new Date().getMilliseconds()}.${extension}`;
+
+
+
+    archivo.mv(`uploads/${tipo}/${nombreArchivo}`, (err) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        // Aqui la imagen se cargo
+
+        if (tipo === 'usuarios') {
+            imagenUsuario(id, res, nombreArchivo);
+        } else {
+            imagenProducto(id, res, nombreArchivo);
+        }
+
+
+    });
+
+});
+
+function imagenUsuario(id, res, nombreArchivo) {
+
+    Usuario.findById(id, (err, usuarioDB) => {
+        if (err) {
+            borraArchivo(nombreArchivo, 'usuarios');
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!usuarioDB) {
+            borraArchivo(nombreArchivo, 'usuarios');
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no existe'
+                }
+            });
+        }
+
+        borraArchivo(usuarioDB.img, 'usuarios');
+
+        usuarioDB.img = nombreArchivo;
+        usuarioDB.save((err, usuarioGuardado) => {
+            res.json({
+                ok: true,
+                usuario: usuarioGuardado,
+                img: nombreArchivo
+            });
+        });
+
+    });
+
+}
+
+function imagenProducto(id, res, nombreArchivo) {
+
+    Producto.findById(id, (err, productoDB) => {
+
+        if (err) {
+            borraArchivo(nombreArchivo, 'productos');
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!productoDB) {
+            borraArchivo(nombreArchivo, 'productos');
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'El producto no existe'
+                }
+            });
+        }
+
+        borraArchivo(productoDB.img, 'productos');
+
+        productoDB.img = nombreArchivo;
+        productoDB.save((err, productoGuardado) => {
+            res.json({
+                ok: true,
+                producto: productoGuardado,
+                img: nombreArchivo
+            });
+        });
+
+    });
+
+}
+
+function borraArchivo(nombreImagen, tipo) {
+
+    const pathImagen = path.resolve(__dirname, `../../uploads/${tipo}/${nombreImagen}`);
+
+    if (fs.existsSync(pathImagen)) {
+        fs.unlinkSync(pathImagen);
+    }
+
+}
+
+module.exports = app;
